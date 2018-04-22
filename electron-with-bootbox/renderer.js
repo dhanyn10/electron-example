7e74f4097a74d437d3c07cab3afbe8aa29acd647
Dhany Nurdiansyah <dhanyn.dhan@gmail.com>
2018-04-22 15:32:18 +0700

    add function for button click with getting input value

--- electron-with-bootbox/renderer.js
+++ electron-with-bootbox/renderer.js
@@ -1,6 +1,14 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+//simple alert
 $("#button-test").click(function(){
     bootbox.alert('button is clicked');
+});
+
+//alert with displaying input
+$("#button-test2").click(function(){
+    form_input = $("#form-input").val();
+    bootbox.alert('your input is : '+ form_input );
 });
