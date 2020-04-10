89d8f5cae4e7a7d72a2ba1ad41575a8b79da57c9
dhanyn10 <dhanyn.dhan@gmail.com>
2020-04-10 23:33:03 +0700

    remove unused var

--- open-directory/renderer.js
+++ open-directory/renderer.js
@@ -1,15 +1,9 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-
 let remote  = require('electron').remote;
-let window  = remote.BrowserWindow;
 let dialog  = remote.dialog;
-let mainapp = remote.require('./main');
 let btn     = document.getElementById('browse');
-
-const url   = require('url');
-const path  = require('path');
 
 btn.addEventListener('click', ()=>{
     //show open directory window
