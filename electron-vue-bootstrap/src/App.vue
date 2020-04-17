6a9be6a42414bc3b2376c5a812986257f8f26a8b
dhanyn10 <dhanyn.dhan@gmail.com>
2020-04-17 18:08:06 +0700

    init electron-vue simple app

---
+++ electron-vue-bootstrap/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Button/>
+  </div>
+</template>
+
+<script>
+import Button from './components/Button.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Button
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ electron-vue-bootstrap/src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <b-button>Button</b-button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { BButton} from 'bootstrap-vue'
+Vue.component('b-button', BButton)
+export default {
+  name: 'Button'
+}
+</script>
