509b8d4cc6ab304044fe035256baba6a72a819bd
dhanyn10 <dhanyn.dhan@gmail.com>
2020-09-17 13:58:41 +0700

    test

--- testing-spectron/main.js
+++ testing-spectron/main.js
@@ -13,7 +13,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
