cc05ca789c8f909b6f3eaac1e0421ae0ff0d020e
dhanyn10 <dhanyn.dhan@gmail.com>
2020-10-16 12:31:00 +0700

    update

--- gdrive/renderer.js
+++ gdrive/renderer.js
@@ -53,29 +53,8 @@ function getAccessToken(oAuth2Client, callback) {
             });
         }
     })
-    // const rl = readline.createInterface({
-    //     input: process.stdin,
-    //     output: process.stdout,
-    // });
-    // rl.question('Enter the code from that page here: ', (code) => {
-    //     rl.close();
-    //     oAuth2Client.getToken(code, (err, token) => {
-    //         if (err) return console.error('Error retrieving access token', err);
-    //         oAuth2Client.setCredentials(token);
-    //         // Store the token to disk for later program executions
-    //         fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-    //             if (err) return console.error(err);
-    //             console.log('Token stored to', TOKEN_PATH);
-    //         });
-    //         callback(oAuth2Client);
-    //     });
-    // });
 }
 
-/**
- * Lists the names and IDs of up to 10 files.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
 function listFiles(auth) {
     const drive = google.drive({version: 'v3', auth});
     drive.files.list({
