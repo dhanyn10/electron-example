40c90c2b7c03c33e873e2b9714a5029952d0c1f6
dhanyn10 <dhanyn.dhan@gmail.com>
2020-10-17 10:33:53 +0700

    update query

--- gdrive/renderer.js
+++ gdrive/renderer.js
@@ -58,13 +58,15 @@ function getAccessToken(oAuth2Client, callback) {
 function listFiles(auth) {
     const drive = google.drive({version: 'v3', auth});
     drive.files.list({
-        pageSize: 10,
+        q: "'root' in parents and mimeType='application/vnd.google-apps.folder'",
+        fields: '*',
+        spaces: 'drive',
         fields: 'nextPageToken, files(id, name)',
     }, (err, res) => {
         if (err) return console.log('The API returned an error: ' + err);
         const files = res.data.files;
         if (files.length) {
-            console.log('Files:');
+            console.log('My Drive(root):');
             files.map((file) => {
                 console.log(`${file.name} (${file.id})`);
             });
