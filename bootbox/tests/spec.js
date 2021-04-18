41828d3daff921dde8418f3e278c98280c6c6b1b
dhanyn10 <dhanyn.dhan@gmail.com>
2021-04-18 17:24:35 +0700

    add mocha and spectron

---
+++ bootbox/tests/spec.js
@@ -0,0 +1,25 @@
+const appplication = require('spectron').Application
+const assert = require('assert')
+const electronPath = require('electron')
+const path = require('path')
+
+describe('Application launch', function () {
+    this.timeout(10000)
+
+    beforeEach(function () {
+        this.app = new appplication({
+            path: electronPath,
+            args: [path.join(__dirname, '..')]
+        })
+        return this.app.start()
+    })
+    afterEach(function() {
+        if(this.app && this.app.isRunning())
+            return this.app.stop()
+    })
+    it('show an initial window', function () {
+        return this.app.client.getWindowCount().then(function (count) {
+            assert.equal(count, 1)
+          })
+    })
+})
